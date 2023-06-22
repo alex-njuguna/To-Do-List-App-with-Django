236209222fa0c1d92f2bba82692197a965c363d4
= <=>
2023-06-22 20:31:03 +0300

    finished functionality

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
-    path('', views.task_list, name='tasks')
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete')
 ]
